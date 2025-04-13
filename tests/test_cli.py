f993448675681b5c0da6deef8f28c6b4eb2f4f3d
Alexandre Gauthier <alex@underwares.org>
2025-04-13 14:02:19 -0400

    Add ruff pass on tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,22 +5,25 @@ from exosphere.commands.test import app as testcommands_cli
 
 runner = CliRunner()
 
+
 def test_repl_testcommands_root() -> None:
     result = runner.invoke(repl_cli, ["test"])
     assert result.exit_code == 0
+
 
 def test_testcommands_greet() -> None:
     result = runner.invoke(testcommands_cli, ["greet"])
     assert result.exit_code == 0
     assert "Hello, World!" in result.stdout
 
+
 def test_testcommands_greet_with_name() -> None:
     result = runner.invoke(testcommands_cli, ["greet", "--name", "Alice"])
     assert result.exit_code == 0
     assert "Hello, Alice!" in result.stdout
 
+
 def test_testcommands_beep() -> None:
     result = runner.invoke(testcommands_cli, ["beep"])
     assert result.exit_code == 0
     assert "Beep!" in result.stdout
-
