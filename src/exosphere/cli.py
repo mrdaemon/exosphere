a8998ed7676a0de5bc341fb9db48d1408dc69af0
Alexandre Gauthier <alex@underwares.org>
2025-06-10 14:41:55 -0400

    Add built in help, win32 readline support
    - Add built in help support for root REPL prompt, for a unified
      experience. Correctly documents commands based on context.
     - Add compat 'readline' module shim using pyreadline3 on windows
     - monkeypatch global 'readline' module on windows within exosphere to use the compat shim.

--- src/exosphere/cli.py
+++ src/exosphere/cli.py
@@ -1,11 +1,27 @@
 import logging
+import sys
+from typing import Annotated, Optional
 
-import typer
+# ------------------win32 readline monkeypatch---------------------
+if sys.platform == "win32":
+    try:
+        from exosphere.compat import win32readline as readline
+
+        sys.modules["readline"] = readline
+    except ImportError:
+        sys.stderr.write(
+            "Warning: pyreadline3 not found. "
+            "Interactive shell may not enable all features.\n"
+        )
+# -----------------------------------------------------------------
+
 from click_shell import make_click_shell
-from rich.console import Console
+from rich import print
+from rich.panel import Panel
+from typer import Argument, Context, Exit, Typer
 
 from exosphere import __version__
-from exosphere.commands import host, inventory, ui
+from exosphere.commands import host, inventory
 
 banner = f"""[turquoise4]
                          ▗▖
@@ -18,20 +34,62 @@ banner = f"""[turquoise4]
                     ▐▌ [green]v{__version__}[/green][/turquoise4]
 """
 
-app = typer.Typer(
+app = Typer(
     no_args_is_help=False,
 )
 
 # Setup commands from modules
 app.add_typer(inventory.app, name="inventory")
-#app.add_typer(ui.app, name="ui") # Ui module disabled until release
+# app.add_typer(ui.app, name="ui") # Ui module disabled until release
 app.add_typer(host.app, name="host")
+
+
+# Help wrapper to use typer's help system
+# Except for the root command, which has its own implementation
+@app.command(hidden=True)
+def help(ctx: Context, command: Annotated[Optional[str], Argument()] = None):
+    msg = "\nUse '<command> --help' or 'help <command>' for help on a specific command."
+    # Show root help if no command is specified
+    if not command:
+        if ctx.parent and getattr(ctx.parent, "command", None):
+            subcommands = getattr(ctx.parent.command, "commands", {})
+            lines = []
+            for name, cmd in subcommands.items():
+                if cmd.hidden:
+                    continue
+                lines.append(
+                    f"[cyan]{name:<11}[/cyan] {cmd.help or 'No description available.'}"
+                )
+            content = "\n".join(lines)
+            panel = Panel.fit(
+                content,
+                title="Commands",
+                title_align="left",
+            )
+            print("\nAvailable modules during interactive use:\n")
+            print(panel)
+        print(msg)
+        return
+
+    # Show command help if one is specified
+    subcommand = None
+    if ctx.parent and getattr(ctx.parent, "command", None):
+        subcommands = getattr(ctx.parent.command, "commands", None)
+        subcommand = subcommands.get(command) if subcommands else None
+        if subcommand:
+            subcommand.get_help(ctx)
+            print(f"\nUse '{str(subcommand.name)} <command> --help' for more details.")
+            return
+
+    # Fall through for unknown commands
+    print(f"[red]Unkown command '{command}'[/red]")
+    print(msg)
 
 
 # The default command fall through call back
 # We use this to start the REPL if no command is given.
 @app.callback(invoke_without_command=True)
-def cli(ctx: typer.Context) -> None:
+def cli(ctx: Context) -> None:
     """
     Exosphere CLI
     """
@@ -40,8 +98,7 @@ def cli(ctx: typer.Context) -> None:
         logger.info("Starting Exosphere REPL interface")
 
         # Print the banner
-        console = Console()
-        console.print(banner)
+        print(banner)
 
         # Start interactive REPL
         repl = make_click_shell(
@@ -49,4 +106,4 @@ def cli(ctx: typer.Context) -> None:
             prompt="exosphere> ",
         )
         repl.cmdloop()
-        typer.Exit(0)
+        Exit(0)
