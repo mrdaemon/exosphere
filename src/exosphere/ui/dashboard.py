8adbd736046f834034e98c5b086943d396d317c0
Alexandre Gauthier <alex@underwares.org>
2025-06-14 17:31:58 -0400

    Add shockingly basic dashboard sketch

--- src/exosphere/ui/dashboard.py
+++ src/exosphere/ui/dashboard.py
@@ -1,10 +1,40 @@
+import logging
+
 from textual.app import ComposeResult
+from textual.containers import Grid
 from textual.screen import Screen
-from textual.widgets import Footer, Header, Placeholder
+from textual.widgets import Footer, Header, Static
+
+from exosphere import context
+
+
+class HostGrid(Grid):
+    """Grid to display hosts in the dashboard."""
+
+    def compose(self) -> ComposeResult:
+        """Compose the grid layout."""
+        inventory = context.inventory
+
+        hosts = inventory.hosts if inventory else []
+        for host in hosts:
+            logging.getLogger("exosphere.ui").debug(f"Adding host: {host.name}")
+            yield Static(host.name, classes="host-box")
 
 
 class DashboardScreen(Screen):
     """Screen for the dashboard."""
+
+    CSS = """
+    HostGrid {
+        grid-size: 3;
+        grid-gutter: 2;
+        align: center middle;
+        width: 100%;
+        height: 100%;
+        content-align: center middle;
+    
+    }
+    """
 
     BINDINGS = [
         ("p", "ping_all_hosts", "Ping All"),
@@ -13,7 +43,7 @@ class DashboardScreen(Screen):
     def compose(self) -> ComposeResult:
         """Compose the dashboard layout."""
         yield Header()
-        yield Placeholder(name="dashboard_content", label="Dashboard Content")
+        yield HostGrid(classes="centered-grid")
         yield Footer()
 
     def on_mount(self) -> None:
