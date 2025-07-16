5e9b163fd5beccf3fb318fc8f3ac56f9d812b2c0
Alexandre Gauthier <alex@underwares.org>
2025-07-16 01:13:03 -0400

    cli security: Implement check command
    Also, misc clean ups

--- src/exosphere/commands/security.py
+++ src/exosphere/commands/security.py
@@ -5,7 +5,8 @@ from rich.console import Console
 from rich.table import Table
 from typing_extensions import Annotated
 
-from exosphere.auth import has_sudo_flag
+from exosphere import app_config, context
+from exosphere.auth import SudoPolicy, check_sudo_policy, has_sudo_flag
 from exosphere.providers.factory import PkgManagerFactory
 
 app = typer.Typer(
@@ -23,6 +24,36 @@ class ProviderInfo:
     class_name: str
     reposync_requires_sudo: bool
     get_updates_requires_sudo: bool
+
+
+# Mapping of package manager names to their friendly descriptions.
+_provider_mapping: dict[str, str] = {
+    "apt": "Debian/Ubuntu Derivatives",
+    "dnf": "Fedora/RHEL/CentOS Derivatives",
+    "yum": "RHEL/CentOS 7 and earlier",
+    "pkg": "FreeBSD",
+}
+
+
+def _get_inventory():
+    """
+    Get the inventory from context
+    A convenience wrapper that bails if the inventory is not initialized
+    """
+    if context.inventory is None:
+        err_console.print(
+            "[red]Inventory is not initialized! Are you running this module directly?[/red]"
+        )
+        raise typer.Exit(1)
+
+    return context.inventory
+
+
+def _get_global_policy() -> SudoPolicy:
+    """
+    Get the default Sudo Policy from the app config.
+    """
+    return SudoPolicy(app_config["options"]["default_sudo_policy"])
 
 
 def _get_provider_infos() -> dict[str, ProviderInfo]:
@@ -62,7 +93,30 @@ def _format_sudo_status(requires_sudo: bool) -> str:
     """
     Format the sudo status for display in the table
     """
-    return "[red]Requires Sudo[/red]" if requires_sudo else "[green]OK[/green]"
+    return (
+        "[red]Requires Sudo[/red]" if requires_sudo else "[green]No Privileges[/green]"
+    )
+
+
+def _format_can_run(
+    can_run: bool,
+) -> str:
+    """
+    Format the policy status for display in the table
+    """
+    return "[green]Yes[/green]" if can_run else "[red]No[/red]"
+
+
+@app.command()
+def policy():
+    """
+    Show the current global Sudo Policy.
+
+    This command will display the current global Sudo Policy that is used
+    to determine if a host can execute all of its Package Manager provider
+    operations.
+    """
+    console.print(f"Global SudoPolicy: {_get_global_policy()}")
 
 
 @app.command()
@@ -71,11 +125,95 @@ def check(
 ):
     """
     Check the security policies for a given host.
+
+    The command will take in consideration the current global Sudo Policy and the
+    host-specific Sudo Policy (if defined) to determine if the host can execute
+    all of its Package Manager provider operations.
     """
-    console.print(f"Checking security policies for host: {host}", style="bold green")
-    console.print(
-        "This is where I would shit whether or not stuff would run for the host"
+
+    # Collect data and sources
+    global_policy = _get_global_policy()
+    inventory = _get_inventory()
+    target_host = inventory.get_host(host)
+
+    if not target_host:
+        err_console.print(f"[red]Host '{host}' not found in inventory![/red]")
+        return
+
+    # Collect sudo policies
+    host_policy: SudoPolicy = target_host.sudo_policy
+    policy_is_local = host_policy != global_policy
+
+    # Collect package manager from host
+    host_pkg_manager_name = target_host.package_manager
+    if not host_pkg_manager_name:
+        err_console.print(
+            f"Host '{host}' does not have a package manager defined in the inventory."
+            " Ensure discovery has been run on the host at least once!"
+        )
+        return
+
+    # Get the package mananager class from the factory registry
+    # We get the raw class to inspect, and do not need/want an instance
+    host_pkg_manager = PkgManagerFactory.get_registry().get(host_pkg_manager_name)
+    if not host_pkg_manager:
+        err_console.print(
+            f"[red]Host '{host}' has an unknown package manager: {host_pkg_manager_name}[/red]"
+            " This is likely a bug and should be reported."
+        )
+        return
+
+    # Gather sudo policy checks
+    can_reposync = check_sudo_policy(host_pkg_manager.reposync, host_policy)
+    can_get_updates = check_sudo_policy(host_pkg_manager.get_updates, host_policy)
+
+    # Output data to console
+    console.print(f"[bold]Sudo Policy for {host}[/bold]")
+    console.print()
+
+    # Prepare a Rich table to display the security policies
+    # We're going to hide most of the table formatting so it just keeps
+    # properties and values vertically aligned with each other.
+    table = Table(
+        "Property",
+        "Value",
+        show_header=False,
+        show_lines=False,
+        box=None,
+        show_edge=False,
+        show_footer=False,
     )
+
+    table.add_row("Global Policy:", str(global_policy))
+
+    if policy_is_local:
+        table.add_row("Host Policy:", f"[cyan]{host_policy}[/cyan] (local)")
+    else:
+        table.add_row("Host Policy:", f"{host_policy} (global)")
+
+    table.add_row("Package Manager:", host_pkg_manager_name)
+
+    table.add_row("", "")  # Blank row for spacing
+
+    table.add_row(
+        "Can Synchronize Catalog:",
+        _format_can_run(can_reposync),
+    )
+    table.add_row(
+        "Can Get Updates:",
+        _format_can_run(can_get_updates),
+    )
+
+    # Display results, with optional warnings
+    console.print(table)
+    console.print()
+
+    if not can_reposync or not can_get_updates:
+        err_console.print(
+            "[yellow]Warning: One or more operations require sudo privileges "
+            "that are not granted by the current policy.\n"
+            "Some functionality may be limited.[/yellow]"
+        )
 
 
 @app.command()
@@ -85,19 +223,19 @@ def providers(
     ] = None,
 ) -> None:
     """
-    List Package Manager Providers in Exosphere and their Security Requirements
+    Show Security Policy requirements for available providers.
 
-    Some providers require sudo provileges to execute certain operations. You can use
-    this command to list them.
+    Some providers require sudo privileges to execute certain operations.
+    You can use this command to list them.
     """
 
     # prepare a nice rich Table for providers
     providers_table = Table(
         "Provider",
-        "Class",
+        "Platform",
         "Refresh Catalog",
         "Refresh Updates",
-        title="Available Providers",
+        title="Providers Requirements",
     )
 
     provider_infos = _get_provider_infos()
@@ -110,8 +248,8 @@ def providers(
 
     for provider in target_providers:
         providers_table.add_row(
-            provider.name,
             provider.class_name,
+            _provider_mapping.get(provider.name, provider.name),
             _format_sudo_status(provider.reposync_requires_sudo),
             _format_sudo_status(provider.get_updates_requires_sudo),
         )
