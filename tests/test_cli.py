879ae837d407e25c5075c82d09eca3a6359e3008
Alexandre Gauthier <alex@underwares.org>
2025-07-29 21:55:56 -0400

    Replace click_shell with custom REPL
    Pull in prompt_toolkit for readline functionality, fancy hooks. Added
    custom command completer for tab completion, readline style, and
    implemented a handful of important features, so we now have:
    
    * tab completion (down to the option/flag)
    * Fancy history (via prompt_toolkit)
    * persistent file history with fallback to in-ram in case of disaster
    * Nicer subcommand error handling than click_shell ever had
    * Welcome message
    * Basic colors (the birth of syntax highlighting?)
    * Life choice regrets

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,44 +3,6 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
-def test_win32readline_monkeypatch(monkeypatch, mocker) -> None:
-    """
-    Test that the windows compatibility shim for pyreadline is
-    correctly applied when running on Windows.
-    """
-    import importlib
-    import sys
-    import types
-
-    # fake sys.platform to be win32
-    monkeypatch.setattr(sys, "platform", "win32")
-
-    # Mock the compatibility module
-    compat_shim = types.ModuleType("exosphere.compat.win32readline")
-    monkeypatch.setitem(sys.modules, "exosphere.compat.win32readline", compat_shim)
-
-    import exosphere.compat
-
-    setattr(exosphere.compat, "win32readline", compat_shim)
-
-    # Clean modules to ensure proper imports
-    sys.modules.pop("readline", None)
-    sys.modules.pop("exosphere.cli", None)
-
-    # Reimport the cli module to trigger the monkeypatch
-    # but patch out codepaths with win32 specific imports.
-    # This is untenable long-term, and this test needs refactored.
-    mocker.patch("ssl.create_default_context")
-
-    import exosphere.cli  # noqa: F401
-
-    importlib.reload(exosphere.cli)
-
-    # Ensure the compatibility shim is used
-    assert "readline" in sys.modules
-    assert sys.modules["readline"] is compat_shim
-
-
 def test_repl_root(mocker, caplog) -> None:
     import logging
 
@@ -49,21 +11,25 @@ def test_repl_root(mocker, caplog) -> None:
     logging.getLogger("exosphere.cli").setLevel(logging.INFO)
     logging.getLogger("exopshere.cli").addHandler(caplog.handler)
 
-    mocker.patch("exosphere.cli.make_click_shell")
+    mock_repl = mocker.patch("exosphere.cli.start_repl")
     result = runner.invoke(repl_cli, [])
-    assert result.exit_code == 0
 
+    assert result.exit_code == 0
+    mock_repl.assert_called_once()
     assert "Starting Exosphere REPL" in caplog.text
 
 
 def test_repl_version(mocker) -> None:
     """
-    Test the 'version' command in the REPL.
-    Should print the version of Exosphere.
+    Test that --version as an argument to the REPL
+    prints the version of Exosphere.
+
+    This is intended to work from command line, but it is handled as a
+    special case inside the REPL hooks.
     """
     from exosphere.cli import app as repl_cli
 
-    mocker.patch("exosphere.cli.make_click_shell")
+    mocker.patch("exosphere.cli.start_repl")
     result = runner.invoke(repl_cli, ["--version"])
     assert result.exit_code == 0
 
@@ -103,50 +69,3 @@ def test_ui_webstart(mocker, caplog) -> None:
     mock_server.return_value.serve.assert_called_once()
 
     assert "Starting Exosphere Web UI Server" in caplog.text
-
-
-def test_help_no_command(mocker):
-    """
-    Test the 'help' command with no arguments.
-    Should make use of our internal help override
-    """
-    from exosphere.cli import app as repl_cli
-
-    # Patch Panel.fit to just return its content for easier assertion
-    mocker.patch("rich.panel.Panel.fit", side_effect=lambda content, **kwargs: content)
-    result = runner.invoke(repl_cli, ["help"])
-    assert result.exit_code == 0
-
-    # Should mention available modules and help usage
-    assert "Available modules during interactive use" in result.output
-    assert "Use '<command> --help'" in result.output
-
-
-def test_help_command(mocker):
-    """
-    Test the 'help' command with a specific subcommand.
-    Should print the help text for that command.
-    """
-    from exosphere.cli import app as repl_cli
-
-    # Patch Panel.fit to just return its content for easier assertion
-    mocker.patch("rich.panel.Panel.fit", side_effect=lambda content, **kwargs: content)
-
-    result = runner.invoke(repl_cli, ["help", "config"])
-    assert result.exit_code == 0
-
-    # Should mention the help text for the 'config' command
-    assert "Configuration related commands" in result.output
-
-
-def test_help_unknown_command(monkeypatch):
-    """
-    Test the 'help' command with an unknown subcommand.
-    Should print an error message.
-    """
-    from exosphere.cli import app as repl_cli
-
-    result = runner.invoke(repl_cli, ["help", "doesnotexist"])
-    assert result.exit_code == 0
-    assert "Unkown command 'doesnotexist'" in result.output
-    assert "Use '<command> --help'" in result.output
