03384a138279f553b9325dcfb6d249e5420001dd
Alexandre Gauthier <alex@underwares.org>
2025-08-17 05:28:59 -0400

    Host: Only set _pkginst if supported on deserialization

--- src/exosphere/objects.py
+++ src/exosphere/objects.py
@@ -142,7 +142,7 @@ class Host:
         # Reset unserializables
         self.logger = logging.getLogger(__name__)
         self._connection = None
-        if "package_manager" in state:
+        if "package_manager" in state and state["supported"]:
             self._pkginst = PkgManagerFactory.create(state["package_manager"])
 
         # Ensure all parameters with defaults are properly set
